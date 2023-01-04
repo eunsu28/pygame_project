fa51dfc59029a1a6fba4c9e230a3ef25c2925c9b
AY_28 <77998267+eunsu28@users.noreply.github.com>
2023-01-04 07:56:17 +0000

    hello

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import pygame, sys
 from pygame.locals import QUIT
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
+DISPLAYSURF = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('Hello World!')
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    #write your code
+    
+    #
     pygame.display.update()
 
 #https://parkjh7764.tistory.com/88
